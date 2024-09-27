74ab5c74df1dc4d04c5582aa3db7608dcfaf47eb
chucrutes <tmelo387@gmail.com>
2024-09-27 00:13:59 -0300

    fix: removing dead code

--- src/infra/http/app.ts
+++ src/infra/http/app.ts
@@ -8,29 +8,11 @@ import { green } from 'console-log-colors'
 import { interceptErrors } from './middlewares/intercept-errors'
 
 export const app = express()
-// const io = new Server({
-//   cors: {
-//     origin: 'http://localhost:3000',
-//     methods: ['GET', 'POST']
-//   }
-// })
 
 app.use(cors())
 app.use(express.json())
 app.get('/', (_, res) => {
   res.status(200).send('OK')
 })
-// io.on('connection', (socket) => {
-//   console.log(green(socket.id))
-//   console.log('a user connected')
-
-//   socket.on('channel', (message: string) => {
-//     console.log(message)
-//     socket.emit('queue', `teste${message}`)
-//   })
-
-//   socket.emit('queue', 'teste')
-// })
-// io.listen(3001)
 app.use('/api', router)
 app.use(interceptErrors)
