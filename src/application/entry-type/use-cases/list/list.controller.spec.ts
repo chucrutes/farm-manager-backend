c8f7f6bde2749dc7c651efbc78c686515d9429d0
Thiago Melo <tmelo387@gmail.com>
2025-01-26 22:07:48 -0300

    refactor: removed logs and added afterAll to prevent unnecessary changes in the future

--- src/application/entry-type/use-cases/list/list.controller.spec.ts
+++ src/application/entry-type/use-cases/list/list.controller.spec.ts
@@ -3,13 +3,12 @@ import type { IFarmsRepository } from '@/application/farms/repositories/IFarmsRe
 import PrismaFarmsRepository from '@/application/farms/repositories/prisma/PrismaFarmsRepository'
 import type { IUsersRepository } from '@/application/users/repositories/IUsersRepository'
 import { PrismaUsersRepository } from '@/application/users/repositories/prisma/PrismaUsersRepository'
-import { beforeAll, describe, expect, test } from 'vitest'
+import { afterAll, beforeAll, describe, expect, test } from 'vitest'
 import { initEntities, ROUTE_ENTITY } from '../../test/init-entities'
 import { app } from '@/infra/http/app'
 import request from 'supertest'
 import type { IEntryTypesRepository } from '../../repositories/IEntryTypesRepository'
 import PrismaEntryTypesRepository from '../../repositories/prisma/PrismaEntryTypesRepository'
-import { stringifier } from '@/core/stringifier'
 
 let usersRepository: IUsersRepository
 let farmsRepository: IFarmsRepository
@@ -22,6 +21,8 @@ describe('List entry types (E2E)', async () => {
     userWithJwt: { user, jwt },
   } = initEntities()
 
+  console.log('list entry types', farm.id, user.id)
+
   beforeAll(async () => {
     usersRepository = new PrismaUsersRepository()
     farmsRepository = new PrismaFarmsRepository()
@@ -30,6 +31,7 @@ describe('List entry types (E2E)', async () => {
     await usersRepository.create(user)
     await farmsRepository.createOrUpdate(farm)
     await farmsRepository.addMember(user.id, farm.id, Roles.OWNER)
+    console.log('farm', farm.id)
 
     const promises = manyEntryTypes.map((item) =>
       entryTypesRepository.createOrUpdate(item),
@@ -45,7 +47,9 @@ describe('List entry types (E2E)', async () => {
     expect((response.body.dto as Array<unknown>).length).toEqual(
       manyEntryTypes.length,
     )
+  })
 
+  afterAll(async () => {
     await farmsRepository.deleteMany([farm.id])
     await usersRepository.delete(user.id)
   })
